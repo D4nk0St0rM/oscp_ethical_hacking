1330e6b1a07447d23cd148453df3fa37db6e1b2c
D4nk0St0rM <76242256+D4nk0St0rM@users.noreply.github.com>
2021-07-21 13:37:49 +0100

    Update quick_n_dirty_ftp.sh

--- ftp/quick_n_dirty_ftp.sh
+++ ftp/quick_n_dirty_ftp.sh
@@ -4,7 +4,8 @@
 sudo groupadd ftpgroup
 sudo useradd -g ftpgroup -d /dev/null -s /etc ftpuser
 sudo pure-pw useradd babycow -u ftpuser -d /ftphome
-sudo pure-pw mkdbsudo cd /etc/pure-ftpd/auth/
+sudo pure-pw mkdb
+sudo cd /etc/pure-ftpd/auth/
 sudo ln -s ../conf/PureDB 60pdb
 sudo mkdir -p /ftphome
 sudo chown -R ftpuser:ftpgroup /ftphome/
