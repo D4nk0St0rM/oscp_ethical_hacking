1a2b93190d2a44398a9ab0e0524bb51ab3737a2f
D4nk0St0rM <76242256+D4nk0St0rM@users.noreply.github.com>
2021-07-21 13:34:22 +0100

    Create quick_n_dirty_ftp.sh

---
+++ ftp/quick_n_dirty_ftp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# sudo apt update && sudo apt install pure-ftpd -y
+sudo groupadd ftpgroup
+sudo useradd -g ftpgroup -d /dev/null -s /etc ftpuser
+sudo pure-pw useradd babycow -u ftpuser -d /ftphome
+sudo pure-pw mkdbsudo cd /etc/pure-ftpd/auth/
+sudo ln -s ../conf/PureDB 60pdb
+sudo mkdir -p /ftphome
+sudo chown -R ftpuser:ftpgroup /ftphome/
+sudo systemctl restart pure-ftpd
+
