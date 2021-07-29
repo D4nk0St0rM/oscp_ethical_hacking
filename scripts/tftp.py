54d774b11f4dab06c2355e37a8ba187f331c35a2
D4nk0St0rM <76242256+D4nk0St0rM@users.noreply.github.com>
2021-07-29 22:30:28 +0100

    Update tftp.py

--- scripts/tftp.py
+++ scripts/tftp.py
@@ -1,6 +1,14 @@
-#!/bin/python
+#!/bin/python3
 
 import tftpy
-client = tftpy.TftpClient(<ip>, <port>)
-client.download("filename in server", "/tmp/filename", timeout=5)
-client.upload("filename to upload", "/local/path/file", timeout=5)
+
+ip = input("Enter IP adress: ")
+po = input("Enter Port: ")
+port = int(po)
+
+def main(ip,port):
+  client = tftpy.TftpClient(ip, port)
+  client.download("filename in server", "/tmp/filename", timeout=5)
+  client.upload("filename to upload", "/local/path/file", timeout=5)
+
+main(ip,port)
