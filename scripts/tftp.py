be2b27539cd0cec8d8691addd404f648dcfd61e9
D4nk0St0rM <76242256+D4nk0St0rM@users.noreply.github.com>
2021-07-29 22:33:13 +0100

    Update tftp.py

--- scripts/tftp.py
+++ scripts/tftp.py
@@ -12,3 +12,6 @@ def main(ip,port):
   client.upload("filename to upload", "/local/path/file", timeout=5)
 
 main(ip,port)
+
+
+#### sudo nmap -sU -p 69 --script tftp-enum.nse --script-args tftp-enum.filelist=/usr/share/nmap/nselib/data/tftplist.txt 10.11.1.111 -vv
