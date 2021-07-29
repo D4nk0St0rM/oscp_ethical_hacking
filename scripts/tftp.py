944da8a799f2830881ce294e3f84e09f0a765536
D4nk0St0rM <76242256+D4nk0St0rM@users.noreply.github.com>
2021-07-29 22:16:56 +0100

    Create tftp.py

---
+++ scripts/tftp.py
@@ -0,0 +1,6 @@
+#!/bin/python
+
+import tftpy
+client = tftpy.TftpClient(<ip>, <port>)
+client.download("filename in server", "/tmp/filename", timeout=5)
+client.upload("filename to upload", "/local/path/file", timeout=5)
